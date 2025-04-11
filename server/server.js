88f08b6b61ecdca72863b1e245347da1b45308c7
Deepanshu44v <deepanshu2070.be21@chitkara.edu.in>
2025-04-11 14:14:50 +0530

    cors final

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://budgettravelling.vercel.app",
     credentials: true,
   })
 );
